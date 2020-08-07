93562d390a1571b4cda9cf6e374f72b1da81c1d8
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-08-07 01:39:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -29,5 +29,16 @@ class Waiter
     
   end
   
+  
+  def best_tipper
+    
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip 
+    end
+    
+    best_tipped_meal.customer
+    
+  end
+  
 
 end
